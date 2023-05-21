84495c9e21c468370746249e92f681d494a63c11
khouloud HADDAD AMAMOU <khouloud.haddad@gmail.com>
2023-05-21 07:25:50 +0100

    Customize create Categories  categories list using filament

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -2,16 +2,21 @@
 
 namespace App\Filament\Resources;
 
+use Closure;
 use App\Filament\Resources\CategoryResource\Pages;
 use App\Filament\Resources\CategoryResource\RelationManagers;
 use App\Models\Category;
 use Filament\Forms;
+use Filament\Forms\Components\Card;
+use Filament\Forms\Components\TextInput;
 use Filament\Resources\Form;
 use Filament\Resources\Resource;
 use Filament\Resources\Table;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Str;
 
 class CategoryResource extends Resource
 {
@@ -23,7 +28,16 @@ class CategoryResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Card::make()->schema([
+                TextInput::make('name')
+                ->reactive()
+                ->required()
+                ->afterStateUpdated(function (Closure $set, $state) {
+                    $set('slug', Str::slug($state));
+                }),
+                TextInput::make('slug')
+                ->required()
+                ])
             ]);
     }
 
@@ -31,7 +45,10 @@ class CategoryResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('id')->sortable(),
+                TextColumn::make('name')->limit(50)->sortable(),
+                TextColumn::make('slug')->limit(50),
+                TextColumn::make('created_at')->dateTime()
             ])
             ->filters([
                 //
--- app/Filament/Resources/CategoryResource/Pages/CreateCategory.php
+++ app/Filament/Resources/CategoryResource/Pages/CreateCategory.php
@@ -9,4 +9,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateCategory extends CreateRecord
 {
     protected static string $resource = CategoryResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
